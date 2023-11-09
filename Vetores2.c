8886845d42d357eafadcf66310295e6314cc1020
Camile Oliveira <143637837+camileoliv@users.noreply.github.com>
2023-11-09 16:29:43 -0300

    Update Vetores2.c

--- Vetores2.c
+++ Vetores2.c
@@ -6,27 +6,19 @@
 
 int main()
 {
-    int numero[5], i,maior,menor;
+    int numero[5], i,maior=0,menor=9999;
 
     for (i = 0; i < 5; i++)
     {
-    printf("Digite o %d número: ", i+1);
+    printf("Digite o %dº número: ", i+1);
     scanf("%d",&numero[i]);
-    }
 
-    maior = numero [0] > numero [1]? numero [0] : numero [1];
-    maior = maior >numero [2]? maior :numero [2];
-    maior = maior >numero [3]? maior :numero [3];
-    maior = maior >numero [4]? maior :numero [4];
-
-    menor = numero [0] <numero [1]? numero [0] :numero [1];
-    menor = menor <numero [2]? menor :numero [2];
-    menor = menor <numero [3]? menor :numero [3];
-    menor = menor <numero [4]? menor :numero [4];
+    maior = numero[i] > maior? numero[i] : maior;
+    menor = numero[i] < menor? numero[i] : menor;
+    }  
 
     printf("A maior numero é: %d \n",maior);
     printf("O menor número é: %d \n",menor);
-
 
     return 0;
 }
