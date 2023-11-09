399d375ff35e03161fb9af18e719fbf3544298ab
Camile Oliveira <143637837+camileoliv@users.noreply.github.com>
2023-11-09 16:55:15 -0300

    Update Vetores3.c
    par e impar

--- Vetores3.c
+++ Vetores3.c
@@ -7,26 +7,28 @@
 int main()
 {
     setlocale(LC_ALL,"portuguese");
-    
+
     int numero[6];
     int i, tamanho=6,parCount=0,imparCount=0;
 
     for (i = 0; i < tamanho; i++)
     {
-    printf("Digite o %iº número: ",i+1);
+    printf("Digite o %iÂº nÃºmero: ",i+1);
     scanf("%d", &numero[i]);
+
+    numero[i] % 2 ==0 ? parCount++ : imparCount++;
     
-     if (numero[i] %2 ==0)
+     /*if (numero[i] %2 ==0)
     {
         parCount ++;
     } else {
         imparCount ++;
-    }
+    }/*/
     
     }
 
     printf("Pares: %d \n",parCount);
-    printf("Ímpares: %d \n",imparCount);
+    printf("Ãmpares: %d \n",imparCount);
 
     return 0;
 }
